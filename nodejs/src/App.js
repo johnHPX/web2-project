a7e9e5bb377378350e0d2b107b73e0367976e5b6
robertscj <robertscj@proton.me>
2026-09-22 13:38:52 -0300

    adicionado App.js

---
+++ nodejs/src/App.js
@@ -0,0 +1,9 @@
+let porta = 8080
+let router = require('./routes/routes.js')
+let middlewares = require('./middlewares/middlewares.js')
+let express = require('express')
+let app = express()
+
+app.listen(porta, (req,res) => {
+    console.log("servidor funcionando na porta", porta)
+})
